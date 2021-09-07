217182e0de88ae6e17ae6c6d65834e2bb700b0fd
alexstouffer <bizkite@hotmail.com>
2021-09-06 20:31:53 -0700

    complete basic webpack integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     // code path to generate an output file
     entry: './assets/js/index.js',
     // where the generated file will go
