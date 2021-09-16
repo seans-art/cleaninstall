5da5c1bd6a9f6f7109019aca133b91718cfe940c
alexstouffer <bizkite@hotmail.com>
2021-09-16 12:19:18 -0700

    change order attributes on photo upload

--- docs/js/uploadcare.js
+++ docs/js/uploadcare.js
@@ -1,35 +1,22 @@
-console.log("scripts appear");
-UPLOADCARE_PUBLIC_KEY = '2281dd5738c1eb6ea46c';
 // import uploadcare from '../../node_modules/uploadcare-widget/uploadcare.js';
+UPLOADCARE_PUBLIC_KEY = '2281dd5738c1eb6ea46c';
 
 // get a widget reference
 const widget = uploadcare.SingleWidget("[role=uploadcare-uploader]");
 
-// listen to the "upload completed" event
-widget.onUploadComplete(fileInfo => {
-  // get a CDN URL from the file info
-  console.log(fileInfo.cdnUrl);
-});
-
-// Select a dropdown value and pass it to the clayer sku attribute
-window.addEventListener('load', function() {
-    console.log('test');
-})
-
-// TODO: put a null default for both options
+// Select a canvas size and return the selected value
 let sizeChoice;
 function selectSize(){
     let size = document.getElementsByName('size-select')[0].value;
-    console.log(size);
     sizeChoice = size;
+    if(colorChoice && sizeChoice){
+        setSku();
+    }
     return sizeChoice;
 }
 document.getElementsByName('size-select')[0].onchange = selectSize;
 
-// Get value from Radio Selectors
-// document.getElementsByName('color-select');
-// document.getElementsByName('color-select')[0].value;
-// document.querySelector('input[name="color-select"]:checked').value;
+// Select a color type and return the selected value
 let colorList = document.getElementsByName('color-select');
 let previous = null;
 let colorChoice;
@@ -38,22 +25,55 @@ for (let i = 0; i < colorList.length; i++) {
         if (this !== previous) {
             previous = this;
         }
-        console.log("current value is: " + this.value)
         colorChoice = this.value;
+        if(colorChoice && sizeChoice){
+            setSku();
+        }
     });
 }
 
-let dataSkuCode;
-function getSku() {
+// Set sku to the joined selected values
+function setSku() {
     if(colorChoice && sizeChoice){
-        console.log(colorChoice.value);
-        console.log(sizeChoice.value);
-    } else {
-        console.log ("one or more values is not set");
+        let btnAddToOrder = document.getElementById('add-to-order');
+        let dataSkuCode = sizeChoice + colorChoice;
+        btnAddToOrder.setAttribute('data-sku-code', dataSkuCode);
     }
 }
 
-let btnAddToOrder = document.getElementById('add-to-order');
-if (dataSkuCode){
-    document.getElementById('add-to-order').setAttribute('data-sku-code', dataSkuCode);
-}
+// Set Order Reference to Photo filename
+document.addEventListener('DOMContentLoaded', function () {
+    // Our Target Node to watch for Mutations
+    const uploadWidget = document.querySelector('#uploadcare--widget__text');
+    
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true, childList: true, subtree: true };
+
+    // Callback function to execute when mutations are observed
+    const callback = function(mutationsList, observer) {
+        // Use traditional 'for loops' for IE 11
+        for(const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                console.log(uploadWidget.innerText);
+            }
+            else if (mutation.type === 'attributes') {
+                console.log('The ' + mutation.attributeName + ' attribute was modified.');
+            }
+        }
+    };
+
+    // Create an observer instance linked to the callback function
+    let observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(uploadWidget, config);
+
+
+
+    uploadWidget.addEventListener('change', function() {
+        let filename = document.querySelector('#uploadcare--widget__text div')?.innerText;
+        if(filename){
+            btnAddToOrder.setAttribute('data-sku-reference', filename);
+        }
+    })    
+}, false);
--- themes/novela/assets/js/uploadcare.js
+++ themes/novela/assets/js/uploadcare.js
@@ -1,8 +1,9 @@
-UPLOADCARE_PUBLIC_KEY = '2281dd5738c1eb6ea46c';
 // import uploadcare from '../../node_modules/uploadcare-widget/uploadcare.js';
+UPLOADCARE_PUBLIC_KEY = '2281dd5738c1eb6ea46c';
 
-// get a widget reference
+// get a widget reference, locate button element, locate filename from photo upload if available
 const widget = uploadcare.SingleWidget("[role=uploadcare-uploader]");
+const btnAddToOrder = document.getElementById('add-to-order');
 
 // Select a canvas size and return the selected value
 let sizeChoice;
@@ -35,8 +36,43 @@ for (let i = 0; i < colorList.length; i++) {
 // Set sku to the joined selected values
 function setSku() {
     if(colorChoice && sizeChoice){
-        let btnAddToOrder = document.getElementById('add-to-order');
         let dataSkuCode = sizeChoice + colorChoice;
         btnAddToOrder.setAttribute('data-sku-code', dataSkuCode);
     }
 }
+
+// Set Order Reference to Photo filename
+document.addEventListener('DOMContentLoaded', function () {
+    // Our Target Node to watch for Mutations
+    const uploadWidget = document.querySelector('#uploadcare--widget__text');
+  
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true, childList: true, subtree: true };
+
+    // Callback function to execute when mutations are observed
+    const callback = function(mutationsList, observer) {
+        const filename = document.querySelector('#uploadcare--widget__text div')?.innerText;
+        const fileUrl = document.querySelectorAll('[role="uploadcare-uploader"]')[0];    
+        // Use traditional 'for loops' for IE 11
+        for(const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                btnAddToOrder.setAttribute('data-sku-reference', filename);
+                btnAddToOrder.setAttribute('data-sku-image-url', fileUrl.value);
+            }
+        }
+    };
+
+    // Create an observer instance linked to the callback function
+    let observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(uploadWidget, config);
+
+
+
+    uploadWidget.addEventListener('change', function() {
+        if(filename){
+            btnAddToOrder.setAttribute('data-sku-reference', filename);
+        }
+    })    
+}, false);
